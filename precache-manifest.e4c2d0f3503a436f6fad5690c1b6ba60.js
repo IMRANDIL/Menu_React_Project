21e1a442a99c09d72b56ffe9057864ad86e108ab
IMRANDIL <aliimranadil2@gmail.com>
2021-06-22 15:52:52 +0530

    Updates

---
+++ precache-manifest.e4c2d0f3503a436f6fad5690c1b6ba60.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "524fa36a8e312bdad16709861db27f8d",
+    "url": "/Menu_React_Project/index.html"
+  },
+  {
+    "revision": "54e6835f4428933e9f0f",
+    "url": "/Menu_React_Project/static/css/main.99af19dc.chunk.css"
+  },
+  {
+    "revision": "6e331cfd5d1a1283573a",
+    "url": "/Menu_React_Project/static/js/2.66b87b03.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Menu_React_Project/static/js/2.66b87b03.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "54e6835f4428933e9f0f",
+    "url": "/Menu_React_Project/static/js/main.dd034831.chunk.js"
+  },
+  {
+    "revision": "eb9d6b65e29d822b0b9f",
+    "url": "/Menu_React_Project/static/js/runtime-main.85b142a4.js"
+  }
+]);
